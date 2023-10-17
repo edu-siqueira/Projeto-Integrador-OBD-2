58b0cf506645ae461d042efcc948ad44b5d5ff81
Nox <eduardo.s.souza@protonmail.com>
2023-10-17 19:25:46 -0300

    Primeiro commit do projeto.

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.relay")
+}
+
+android {
+    namespace = "com.example.projeto_pi2"
+    compileSdk = 34
+
+    SourceSets{
+        main{
+            assets{
+                srcDir("src/main/ui-packages")
+            }
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.projeto_pi2"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    val nav_version = "2.5.3"
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation ("androidx.navigation:navigation-compose:2.7.0-rc01")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+class SourceSets(function: () -> Unit) {
+
+}
+
+fun main(unit: () -> Unit) {
+
+}
+
+fun assets(function: () -> Unit) {
+
+}
+
+fun srcDir(s: String) {
+
+}
---
+++ app/src/main/java/com/example/projeto_pi2/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.projeto_pi2
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.projeto_pi2.frame1.Frame1
+import com.example.projeto_pi2.frame2.Frame2
+import com.example.projeto_pi2.frame3.Frame3
+import com.example.projeto_pi2.frame4.Frame4
+import com.example.projeto_pi2.ui.theme.Projetopi2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Projetopi2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "tela_inicial"
+                    ) {
+                        composable("tela_inicial", ) {
+                            EnterAnimation {
+                                Frame1(emailTextContent = "Email",
+                                    senhaTextContent = "Senha",
+                                    entrarTextContent = "Entrar",
+                                    sejaBemVindoTextContent = "Seja Bem-Vindo!",
+                                    esqueceuASenhaTextContent = "esqueceu a senha?",
+                                    criarUmaContaTextContent = "Criar uma conta",
+                                    onClick = {navController.navigate("tela_principal")}
+                                )
+                            }
+                        }
+                        composable("tela_principal") {
+                            EnterAnimation {
+                                Frame2(errorsCountTextContent = "3 erro(s) detectados!",
+                                    voltsTextContent = "14.5 Volts",
+                                    rpmTextContent = "1.190",
+                                    param67TextContent = "67%",
+                                    onSettingsClick = {navController.navigate("tela_de_opcoes")},
+                                    onErrorClick = {navController.navigate("tela_de_erros")},)
+                            }
+                        }
+                        composable("tela_de_opcoes"){
+                            EnterAnimation {
+                                Frame3(onSettingsClick = {navController.navigate("tela_principal")})
+                            }
+                        }
+                        composable("tela_de_erros"){
+                           EnterAnimation {
+                               Frame4(onSettingsClick = {navController.navigate("tela_principal")})
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Projetopi2Theme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun EnterAnimation(content: @Composable () -> Unit) {
+    AnimatedVisibility(
+        visibleState = MutableTransitionState(
+            initialState = false
+        ).apply { targetState = true },
+        modifier = Modifier,
+        enter = slideInVertically(
+            initialOffsetY = { -40 }
+        ) + expandVertically(
+            expandFrom = Alignment.Top
+        ) + fadeIn(initialAlpha = 0.3f),
+        exit = slideOutVertically() + shrinkVertically() + fadeOut(),
+    ) {
+        content()
+    }
+}
+
+
